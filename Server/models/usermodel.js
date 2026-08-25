7e77a0684d572eed9325f1db9bff956ed88bbb0f
Rishav250-cmd <rishavdev.05@gmail.com>
2026-08-25 12:27:33 +0530

    adding authcontroller , usermodel and different utils

---
+++ Server/models/usermodel.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        name: { type: String, required: true, trim: true },
+        email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+        password: { type: String, minlength: 6, required: true },
+        phone: { type: String, minlength: 6, required: true },
+        nationality: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+userSchema.set("toJSON", {
+    transform: (doc, ret) => {
+        delete ret.password;
+        return ret;
+    },
+});
+
+export const user = model("user", userSchema);
